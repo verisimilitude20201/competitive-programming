30a6c567a475ee5e20727ccc10b6badd62c8a6d5
Abhijit Ghatnekar <aghatnekar@qualys.com>
2020-10-11 08:34:34 +0530

    2 Sums

---
+++ Leet_Coding/Arrays/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,16 @@
+"""
+This is similar to two-sums.py. The only difference is we are incrementing loop counter by 1.
+Since either index1 or index2 should not be zero.
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        A = nums
+        map_difference_to_index = {}
+        for loop_counter in range(0, len(A)):
+            difference = target - A[loop_counter]
+            if A[loop_counter] in map_difference_to_index:
+                diff_indexes = map_difference_to_index[A[loop_counter]]
+                diff_indexes.append(loop_counter + 1)
+                return diff_indexes
+            map_difference_to_index[difference] = [loop_counter + 1]

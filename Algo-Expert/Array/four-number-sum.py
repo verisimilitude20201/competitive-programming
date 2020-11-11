30902a57d58a25d4317b743464f87336c9a1dfdf
Abhijit Ghatnekar <aghatnekar@qualys.com>
2020-11-11 23:02:57 +0530

    Array Problems

--- Algo-Expert/Array/four-number-sum.py
+++ Algo-Expert/Array/four-number-sum.py
@@ -1,4 +1,27 @@
 """
+Approach
+--------
+quadruplets = []
+pairs_of_sums = {}
+
+1. From i in 1st element of array to second last element
+1.1     From j in i + 1th element to last element of the array
+1.2.            current_sum = array[i] + array[j]
+1.3              difference = targetSum - currentSum
+1.4              if difference in hashtable pairs_of_sums
+1.4.1                   append array[i] and array[j] and pair to quadruplets
+1.5    From k in 0 to i
+1.5.1           current_sum = array[i] + array[k]
+1.5.2           If current_sum in pairs_of_sums hash table
+1.5.3                 Append the list [array[i], array[k]] to pairs_of_sums[current_sum]
+1.5.4           Else
+1.5.5                 Set pairs_of_sums[current_sum] = [[array[i], array[k]]]
+
+Complexity
+---------
+O(N^2) Average time complexity
+O(N^2) Space Complexity
+
 """
 def fourNumberSum(array, targetSum):
     quadruplets = []

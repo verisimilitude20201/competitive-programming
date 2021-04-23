6d455854e2c3a0453cb3be8cd04798fa1410e898
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2021-04-24 00:11:53 +0530

    Problems 24/04

---
+++ Common_Coding_Patterns/Reverse_a_Linked_List/reverse_a_linkedlist.py
@@ -0,0 +1,62 @@
+"""
+Problem:
+-------
+Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
+
+Example:
+-------
+2 -> 4 -> 6 -> 8 -> 10 -> None
+
+None <- 2 <- 4 <- 6  <- 8 <- 10 
+
+
+Complexity
+---------
+Time: O(N)
+Space: O(1)
+
+"""
+
+from __future__ import print_function
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+def reverse(head):
+    prev_ptr = None
+    current = head
+    while current is not None:
+        next_ptr = current.next
+        current.next = prev_ptr
+        prev_ptr = current
+        current = next_ptr
+
+    return prev_ptr
+
+
+def main():
+    head = Node(2)
+    head.next = Node(4)
+    head.next.next = Node(6)
+    head.next.next.next = Node(8)
+    head.next.next.next.next = Node(10)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse(head)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+main()

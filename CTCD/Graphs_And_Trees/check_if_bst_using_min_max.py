32ff3624db834912e9d029b0279c038070fa2544
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2021-09-27 19:24:58 +0530

    Problems

---
+++ CTCD/Graphs_And_Trees/check_if_bst_using_min_max.py
@@ -0,0 +1,47 @@
+"""
+Explanation:
+-----------
+In-order traversal and maintain only the last traversed node's value
+
+Complexity:
+---------
+Time: O(N) (We touch all nodes)
+Space: O(log N)
+"""
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        print("Value of node: " + str(self.value))
+
+
+class IsBst:
+    def __init__(self):
+        pass
+
+    def check_if_bst_using_min_max(self, root, min1=None, max1=None):
+        if root is None:
+            return True
+
+        if (min1 is not None and min1 >= root.value) or (max1 is not None and max1 < root.value):
+            return False
+
+        if not self.check_if_bst_using_min_max(root.left, min1, root.value) or not self.check_if_bst_using_min_max(root.right, root.value, max1):
+            return False
+
+        return True
+
+
+def main():
+    root = TreeNode(2)
+    root.left = TreeNode(1)
+    root.right = TreeNode(3)
+    is_bst = IsBst()
+    print(is_bst.check_if_bst_using_min_max(root))
+
+
+main()

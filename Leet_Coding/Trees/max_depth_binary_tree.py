dc532901be73cbf9a053b3abfd3cd8929d943156
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2022-01-01 23:02:00 +0530

    Problems

--- Leet_Coding/Trees/max_depth_binary_tree.py
+++ Leet_Coding/Trees/max_depth_binary_tree.py
@@ -23,3 +23,18 @@ class Solution:
         left_depth = self.maxDepthRecursive(root.left) + 1
         right_depth = self.maxDepthRecursive(root.right) + 1
         return max(left_depth, right_depth)
+    
+    def maxDepthIterative(self, root: Optional[TreeNode]) -> int:
+        depth = 0
+        stack = []
+        if root is not None:
+            stack = [(1, root)]
+        while stack:
+            current_depth, node = stack.pop()
+            depth = max(depth, current_depth)
+            if root.left:
+                stack.append((current_depth + 1, root.left))
+            if root.right:
+                stack.append((current_depth + 1, root.right))
+        
+        return depth

d6e74761e5346a3455eb4b423469bf29a1e55ee3
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2022-02-14 09:41:45 +0530

    Problem

---
+++ Leet_Coding/Stack_and_Queues/649_moving_averages_from_data_streams.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+class MovingAverage:
+    def __init__(self, size: int):
+        self._size = size
+        self._queue = deque([None] * self._size)
+
+    def next(self, val: int) -> float:
+        if len(self._queue) == self._size:
+            self._queue.popleft()
+        self._queue.append(val)
+        total = 0
+        count_of_elements = 0
+        for i in range(len(self._queue)):
+            if self._queue[i] is not None:
+                total += self._queue[i]
+                count_of_elements += 1
+        return total / count_of_elements

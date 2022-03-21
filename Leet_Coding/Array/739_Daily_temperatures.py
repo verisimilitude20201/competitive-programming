a8b24579b0cc3957283b16a9a46652e52e46975b
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2022-03-21 18:11:20 +0530

    Problems

--- Leet_Coding/Array/739_Daily_temperatures.py
+++ Leet_Coding/Array/739_Daily_temperatures.py
@@ -1,8 +1,22 @@
 """
 Complexity:
 ----------
+dailyTemperatures
+-----------------
 Time: O(N^2)
 Space: O(N)
+
+dailyTemperatures1
+------------------
+Time: O(N) Since every element could be added to Stack only once.
+Space: O(N)
+
+
+dailyTemperatures3
+------------------
+Time: O(N) --> No two indexes are visited twice.
+Space: O(1)
+
 """
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
@@ -12,6 +26,33 @@ class Solution:
                 if temperatures[day] < temperatures[future_day]:
                     answers[day] = future_day - day
                     break
+
+        return answers
+    
+    def dailyTemperatures1(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answers = [0] * n
+        stack = []
+        for current_day, temperature in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temperature:
+                prev_day = stack.pop()
+                answers[prev_day] = current_day - prev_day
+            stack.append(current_day)
+        
+        return answers
+    
+    def dailyTemperatures3(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        hottest = 0
+        answers = [0] * n
+        for current_day in range(n - 1, -1, -1):
+            if hottest <= temperatures[current_day]:
+                hottest = temperatures[current_day]
+                continue
+            future_day = 1
+            while temperatures[future_day + current_day] <= temperatures[current_day]:
+                future_day += answers[future_day + current_day]
+            answers[current_day] = future_day
 
         return answers
 
